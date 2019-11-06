496e486344dbed6da2442f788b4c609275e4bf50
Joonas Harjukallio <harjukallio@gmail.com>
2019-11-06 14:44:19 +0200

    Better

--- src/main/kotlin/com/example/grpcserviceclientdemo/app/grpc/AnimalClient.kt
+++ src/main/kotlin/com/example/grpcserviceclientdemo/app/grpc/AnimalClient.kt
@@ -9,39 +9,36 @@ import org.springframework.stereotype.Service
 @Service
 class AnimalClient {
     fun all(): List<AnimalDTO> {
-        val channel: ManagedChannel = ManagedChannelBuilder
-                .forAddress("localhost", 6565)
-                .usePlaintext()
-                .build()
-        val stub: AnimalServiceGrpc.AnimalServiceBlockingStub = AnimalServiceGrpc
-                .newBlockingStub(channel)
-        return try {
+        return execute { stub ->
             val request = AnimalOuterClass.AnimalRequest.newBuilder().build()
             val response = stub.getAnimals(request)
-            return response.animalList.map { AnimalDTO(it.name, it.color, it.countryList) }
-        } catch (e: Exception) {
-            println("Joq meni pielee")
-            throw e
-        } finally {
-            channel.shutdown()
+            response.animalList.map { AnimalDTO(it.name, it.color, it.countryList) }
         }
     }
 
     fun show(id: Int): AnimalDTO {
+        return execute { stub ->
+            val request = AnimalOuterClass.AnimalRequest.newBuilder().setId(id.toString()).build()
+            val response = stub.getAnimals(request)
+            response.animalList.map { AnimalDTO(it.name, it.color, it.countryList) }.first()
+        }
+    }
+
+    fun <T> execute(statement: (stub: AnimalServiceGrpc.AnimalServiceBlockingStub) -> T): T {
         val channel: ManagedChannel = ManagedChannelBuilder
                 .forAddress("localhost", 6565)
                 .usePlaintext()
                 .build()
         val stub: AnimalServiceGrpc.AnimalServiceBlockingStub = AnimalServiceGrpc
                 .newBlockingStub(channel)
+
         return try {
-            val request = AnimalOuterClass.AnimalRequest.newBuilder().setId(id.toString()).build()
-            val response = stub.getAnimals(request)
-            return response.animalList.map { AnimalDTO(it.name, it.color, it.countryList) }.first()
+            statement(stub)
         } catch (e: Exception) {
             println("Joq meni pielee")
             throw e
         } finally {
+            println("SHUTDOWN")
             channel.shutdown()
         }
     }
