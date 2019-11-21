e3a4c15318197ac5160f517663b0f28a6f471144
Joonas Harjukallio <harjukallio@gmail.com>
2019-11-21 12:35:46 +0200

    Better

--- src/main/kotlin/com/example/grpcserviceclientdemo/app/controllers/CountryController.kt
+++ src/main/kotlin/com/example/grpcserviceclientdemo/app/controllers/CountryController.kt
@@ -1,7 +1,6 @@
 package com.example.grpcserviceclientdemo.app.controllers
 
 import com.example.grpcserviceclientdemo.app.grpc.Country
-import com.example.grpcserviceclientdemo.app.grpc.CountryDTO
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,8 +9,5 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/countries")
 class CountryController(val country: Country) {
     @GetMapping("")
-    fun index(): List<CountryDTO> {
-        val countries = country.all()
-        return countries
-    }
+    fun index() = country.all()
 }
