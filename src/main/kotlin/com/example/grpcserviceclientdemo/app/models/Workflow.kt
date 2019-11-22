c498fa76ae7f3624f428c2b7a0ef93628b5095ea
Joonas Harjukallio <harjukallio@gmail.com>
2019-11-22 14:04:56 +0200

    Refac

--- src/main/kotlin/com/example/grpcserviceclientdemo/app/models/Workflow.kt
+++ src/main/kotlin/com/example/grpcserviceclientdemo/app/models/Workflow.kt
@@ -4,39 +4,40 @@ import com.example.grpcservicedemo.grpc.WorkflowOuterClass
 import com.example.grpcservicedemo.grpc.WorkflowServiceGrpc
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
-import io.grpc.stub.AbstractStub
 import org.springframework.stereotype.Service
 
 // testChannel is needed purely for testing purposes at this stage
 @Service
 class Workflow(private val testChannel: ManagedChannel? = null) {
-    fun all() = execute({ channel -> WorkflowServiceGrpc.newBlockingStub(channel) }) { stub ->
+    fun all() = execute { channel ->
+        val stub = WorkflowServiceGrpc.newBlockingStub(channel)
         val request = WorkflowOuterClass.WorkflowsRequest.newBuilder().build()
         val response = stub.getWorkflows(request)
         response.workflowList.map { WorkflowDTO(it.id, it.viewId, it.folder, it.name) }
     }
 
-    fun allAsync() = execute({ channel -> WorkflowServiceGrpc.newFutureStub(channel) }) { stub ->
+    fun allAsync() = execute { channel ->
+        val stub = WorkflowServiceGrpc.newFutureStub(channel)
         val request = WorkflowOuterClass.WorkflowsRequest.newBuilder().build()
         val response = stub.getWorkflows(request)
         response.get().workflowList.map { WorkflowDTO(it.id, it.viewId, it.folder, it.name) }
     }
 
-    fun find(id: Long) = execute({ channel -> WorkflowServiceGrpc.newBlockingStub(channel) }) { stub ->
+    fun find(id: Long) = execute { channel ->
+        val stub = WorkflowServiceGrpc.newBlockingStub(channel)
         val request = WorkflowOuterClass.WorkflowRequest.newBuilder().setId(id).build()
         val response = stub.getWorkflow(request)
         response.workflow.let { WorkflowDTO(it.id, it.viewId, it.folder, it.name) }
     }
 
-    private fun <T, Y : AbstractStub<Y>> execute(generator: (channel: ManagedChannel) -> Y, statement: (stub: Y) -> T): T {
+    private fun <T> execute(statement: (channel: ManagedChannel) -> T): T {
         val channel = this.testChannel ?: ManagedChannelBuilder
                 .forAddress("localhost", 6565)
                 .usePlaintext()
                 .build()
 
-        val stub = generator(channel)
         return try {
-            statement(stub)
+            statement(channel)
         } catch (e: Exception) {
             println("Joq meni pielee")
             throw e
