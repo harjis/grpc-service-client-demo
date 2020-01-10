be91fda02e4df48a863b9a7e146a17aba84d7960
Joonas Harjukallio <harjukallio@gmail.com>
2020-01-10 10:48:21 +0200

    Not needed

--- src/test/kotlin/com/example/grpcserviceclientdemo/app/migrators/WorkflowMigratorTest.kt
+++ src/test/kotlin/com/example/grpcserviceclientdemo/app/migrators/WorkflowMigratorTest.kt
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
 class WorkflowMigratorTest {
     private lateinit var workflowMigrator: WorkflowMigrator
